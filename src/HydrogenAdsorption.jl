d53a0d86cd91d87f859f35c171a1886537243681
Kieraan <kieran1hconradie@gmail.com>
2025-06-17 16:26:19 -0400

    feat: export ideal gas equation

--- src/HydrogenAdsorption.jl
+++ src/HydrogenAdsorption.jl
@@ -8,4 +8,8 @@ export coefficient_matrix
 include("adsorption_systems.jl")
 export IsothermParameters, MDAParameters, DAParameters, MaterialProperties, GeometricParameters, OperationalParameters, AdsorptionParameters, 
 adsorption_isotherm, MDA_adsorption!
+
+include("equations_of_state.jl")
+export ideal_gas_equation
+
 end
