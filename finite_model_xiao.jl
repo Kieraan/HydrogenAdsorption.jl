e87afbb0f052827c92a31d7d53fd14d5ac67e728
Kieraan <kieran1hconradie@gmail.com>
2025-10-07 12:21:23 -0300

    Reverting back to LP params

--- finite_model_xiao.jl
+++ finite_model_xiao.jl
@@ -5,46 +5,64 @@ using Statistics
 using Plots
 using CSV
 using DataFrames
+VERBOSE = false # Set to true to print more information during the simulation
 
 # parameters
 # Tank parameters
-R_T = 0.0508            # Radius of the tank / m
-L = 0.454                # Length of the tank / m
+V = 2.4946e-3 # Volume of tank / m³
+L = 0.4 # Length of the tank / m
 
 # Inputs needed for the coefficient_matrix function
-V = π * R_T^2 * L       # Volume of tank / m³
-dr = 0.00025 / 2         # Radial step size / m
-U = 36                  # Heat transfer coefficient / W/(m²·K)
-T₀ = 281.0              # Initial temperature of the tank / K
-T_air = 281.0           # Ambient temperature / K
-T_H2 = 297.6            # Temperature of the incoming hydrogen gas / K
+R_T = sqrt(V / (pi * L)) # Radius of the tank / m
+dr = (1/2)^2 * 0.0025 / 2 # Radial step size / m
+# k_eff = 0.4304 # Effective thermal conductivity / W/(m·K)
+# U = 36 # Heat transfer coefficient / W/(m²·K)
+U = 36 # Heat transfer coefficient / W/(m²·K)
+
+T₀ = 281.0 # Initial temperature of the tank / K
+T_air = 281.0 # Ambient temperature / K
+# T_air = 282.5 # Ambient temperature / K
+
+#T_H2 = 281.6 # Temperature of the incoming hydrogen gas / K
+T_H2 = 297.6 # Data from finite element model / K
+
+# 
 
 # Isotherm parameters
 # Modified Dubinin-Astakov Isotherm parameters
-α = 3080.0              # Enthalpic Factor / J/mol
-β = 18.9                # Entropic Factor / J/mol K
-m = 2.0                 # Exponential factor
-p₀ = 1470e6             # Saturation pressure / Pa
-n₀ = 71.6               # Limit adsoption / mol/kg
+α = 3080.0 # Enthalpic Factor / J/mol
+β = 18.9 # Entropic Factor / J/mol K
+m = 2.0 # Exponential factor
+p₀ = 1470e6 # Saturation pressure / Pa
+n₀ = 71.6 # Limit adsoption / mol/kg
 
 MDA_params = MDAParameters(n₀, p₀, α, β, m)
 
+# Dubinin-Astakov parameters
+P_lim = 77.75e6     # Pa
+ψ = 7.3235          # mmol g-1
+β = -0.0088         # mol kg-1 K-1
+κ = 772.92          # J mol-1
+γ = 18.828
+m = 2.0            # Exponent in the isotherm equation
+DA_params = DAParameters(P_lim, ψ, β, κ, γ, m)
+
 # Material properties
 # Activated carbon properties
-ρₛ = 269                    # Density of activated carbon / kg/m³
-cₛ = 825                    # Specific heat capacity of carbon / J/kg K
-kₛ = 0.764                  # Thermal conductivity of activated carbon / W/m K
-ε_b = 0.49                 # Bed porosity
-mₛ = (1 - ε_b) * ρₛ * V      # Mass of activated carbon / kg
+ρₛ = 517.6 # Density of activated carbon / kg/m³
+cₛ = 825 # Specific heat capacity of carbon / J/kg K
+mₛ = 0.671 # Mass of activated carbon / kg
+kₛ = 0.646 # Thermal conductivity of activated carbon / W/m K
+ε_b = 0.49 # Bed porosity
 
 # Hydrogen properties
-cₚ = 14700.0            # Specific heat capacity of hydrogen / J/kg K
-cᵥ = 10134.0            # Specific heat capacity at constant volume / J/kg K
-M_H2 = 2.0159e-3        # Molar mass of hydrogen / kg/mol
-R = 8.314               # Ideal gas constant / J/mol K
-k_g = 0.206             # Thermal conductivity of hydrogen / W/m K
+cₚ = 14700.0 # Specific heat capacity of hydrogen / J/kg K
+cᵥ = 10134.0 # Specific heat capacity at constant volume / J/kg K
+M_H2 = 2.0159e-3 # Molar mass of hydrogen / kg/mol
+R = 8.314 # Ideal gas constant / J/mol K
+k_g = 0.206 # Thermal conductivity of hydrogen / W/m K
 
-k_eff = 1 * (kₛ * (1 - ε_b) + k_g * ε_b) # Effective thermal conductivity / W/(m·K)
+k_eff = (kₛ * (1 - ε_b) + k_g * ε_b) # Effective thermal conductivity / W/(m·K)
 n_r, r_span, A, b = coefficient_matrix(R_T, dr, k_eff, U, T_air)
 material_props = MaterialProperties(ρₛ, cₛ, mₛ, kₛ, ε_b, cₚ, cᵥ, M_H2, R, k_g, k_eff)
 
@@ -189,10 +207,15 @@ end
 println("Total mass of hydrogen injected: $(m_in * t_f * 1000) g")
 
 # Experimental data
+# Temperature profiles
 df_exp_middle_temp = CSV.read("inputs/xiao_middle_temp_finite_element.csv", DataFrame)
 df_exp_center_temp = CSV.read("inputs/xiao_center_temp_finite_element.csv", DataFrame)
 df_exp_wall_temp = CSV.read("inputs/xiao_wall_temp_finite_element.csv", DataFrame)
+
+# Pressure profile
 df_exp_pressure = CSV.read("inputs/xiao_pressure_finite_element.csv", DataFrame)
+
+# Mass profiles
 df_exp_gas_mass = CSV.read("inputs/xiao_gas_mass_finite_element.csv", DataFrame)
 df_exp_adsorbed_mass = CSV.read("inputs/xiao_adsorbed_mass_finite_element.csv", DataFrame)
 df_exp_total_mass = CSV.read("inputs/xiao_total_mass_finite_element.csv", DataFrame)
@@ -201,15 +224,15 @@ plt = plot(size=(1600, 900), margin=Plots.cm)
 plt = plot(plt, xlabel="Time / s", ylabel="Temperature / K", title="Temperature Profiles")
 
 # Middle temperature
-plot!(plt, df_exp_middle_temp.Time, df_exp_middle_temp.Temperature, label="Experimental Middle Temperature", color=:black, marker=:circle)
+plot!(plt, df_exp_middle_temp.Time, df_exp_middle_temp.Temperature, label="Experimental Middle Temperature", color=:black, marker=:circle, linestyle=:dash)
 plot!(plt, t, T_middle, label="Simulated Middle Temperature", color=:black, lw=2)
 
 # Center temperature
-plot!(plt, df_exp_center_temp.Time, df_exp_center_temp.Temperature, label="Experimental Center Temperature", color=:blue, marker=:circle)
+plot!(plt, df_exp_center_temp.Time, df_exp_center_temp.Temperature, label="Experimental Center Temperature", color=:blue, marker=:circle, linestyle=:dash)
 plot!(plt, t, T_center, label="Simulated Center Temperature", color=:blue, lw=2)
 
 # Wall temperature
-plot!(plt, df_exp_wall_temp.Time, df_exp_wall_temp.Temperature, label="Experimental Wall Temperature", color=:red, marker=:circle)
+plot!(plt, df_exp_wall_temp.Time, df_exp_wall_temp.Temperature, label="Experimental Wall Temperature", color=:red, marker=:circle, linestyle=:dash)
 plot!(plt, t, T_radius, label="Simulated Wall Temperature", color=:red, lw=2)
 display(plt)
 
